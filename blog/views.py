88b43e93a60cc8bf08ff2a7ce2b218d26e41731d
xhosein083 <xhosein.83@gmail.com>
2025-06-29 18:52:24 +0330

    توضیح درباره تغییرات

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,7 @@ from django.shortcuts import render
 from .models import Post, Project, AboutMe
 
 def home(request):
-    posts = Post.objects.order_by('-created_at')[:5]  # آخرین 5 پست
-    projects = Project.objects.order_by('-created_at')[:5]
-    about = AboutMe.objects.first()
-    return render(request, 'blog/home.html', {'posts': posts, 'projects': projects, 'about': about})
+    return render(request, 'blog/home.html')
 
 def resume_page(request):
     return render(request, 'blog/resume.html')

5aae604094d573f9d46a0c71573f15e67e8a3a99
xhosein083 <xhosein.83@gmail.com>
2025-06-29 18:46:53 +0330

    توضیح درباره تغییرات

--- myblog/settings.py
+++ myblog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=i5^5_3j0x#^=z2_cd+ds03_552+u)b*lo(b$*7l#34@80t9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.onrender.com']
 
